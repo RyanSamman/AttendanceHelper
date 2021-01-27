11029dfc6e05eac2ee261df5e8e87cab6846a526
RyanSamman <RyanSamman@outlook.com>
2021-01-27 11:27:54 +0300

    Popup Javascript entrypoint

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import App from './popup/App';
 
-import App from "./App";
-import "./popup.css";
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 var mountNode = document.getElementById("popup");
 ReactDOM.render(<App />, mountNode);
