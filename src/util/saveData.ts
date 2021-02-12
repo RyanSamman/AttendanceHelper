d246c2c89736cdbbbeb44f8058e76db91601c6e2
RyanSamman <RyanSamman@outlook.com>
2021-02-12 15:37:58 +0300

    Save Data Utility Function
    A workaround to allow files to be downloaded on the frontend without any API

--- src/util/saveData.ts
+++ src/util/saveData.ts
@@ -1,13 +1,13 @@
-let saveData = (data: JSON, fileName: string) => { 
-    var a = document.createElement("a"); 
-    document.body.appendChild(a); 
-    a.setAttribute('style', "display: none"); 
-    let json = JSON.stringify(data, null, 2), 
-    blob = new Blob([json], {type: "octet/stream"}), 
-    url = window.URL.createObjectURL(blob); 
-    a.href = url; 
-    a.download = fileName; 
-    a.click(); 
-    window.URL.revokeObjectURL(url); 
+export default function saveData(data: Object, fileName: string) { 
+    const a = document.createElement("a")
+    document.body.appendChild(a)
+    a.setAttribute('style', "display: none")
+    const json = JSON.stringify(data, null, 2)
+    const blob = new Blob([json], {type: "octet/stream"})
+    const url = window.URL.createObjectURL(blob)
+    a.href = url
+    a.download = fileName
+    a.click()
+    window.URL.revokeObjectURL(url)
     a.remove()
 }
