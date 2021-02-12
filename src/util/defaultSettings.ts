8027af6e90081572d9a1ae285a82e1db6625e8a9
RyanSamman <RyanSamman@outlook.com>
2021-02-12 15:38:48 +0300

    Centralize Default Settings

---
+++ src/util/defaultSettings.ts
@@ -0,0 +1,10 @@
+export const defaultSettings: Settings = {
+	isOpen: false,
+	messagesId: "#chatPlaybackList",
+	developerMessagesId: "#chatPlaybackList",
+	popupId: "#side-panel-open",
+	download: false,
+	failPercent: 0.5,
+	developerMode: false,
+	messagesIndex: undefined
+}
